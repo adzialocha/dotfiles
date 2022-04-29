ac347912949f6b7d3ec39fe7eba89dd04fd8cba0
Andreas Dzialocha <x12@adz.garden>
2022-04-29 22:34:11 +0200

    Add lsp action to keymap

--- config/nvim/lua/keymaps.lua
+++ config/nvim/lua/keymaps.lua
@@ -25,8 +25,9 @@ utils.map {'n', '<Leader>f', ':Files ~<CR>', noremap = false}
 
 -- lsp
 utils.map {'n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>'}
-utils.map {'n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>'}
+utils.map {'n', 'ga', '<cmd>lua vim.lsp.buf.code_action()<CR>'}
 utils.map {'n', 'gf', '<cmd>lua vim.lsp.buf.formatting()<CR>'}
-utils.map {'n', 'cd', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>'}
-utils.map {'n', '<C-j>', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>'}
-utils.map {'n', '<C-k>', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>'}
+utils.map {'n', 'cd', '<cmd>lua vim.diagnostic.open_float()<CR>'}
+utils.map {'n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>'}
+utils.map {'n', '<C-j>', '<cmd>lua vim.diagnostic.goto_prev()<CR>'}
+utils.map {'n', '<C-k>', '<cmd>lua vim.diagnostic.goto_next()<CR>'}
