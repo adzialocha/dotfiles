35a7326f6b516469061692e97e5d1fa3698a5cc1
adz <x12@adz.garden>
2023-12-20 14:20:36 +0100

    Show hidden files when searching with telescope

--- config/nvim/lua/plugins/telescope.lua
+++ config/nvim/lua/plugins/telescope.lua
@@ -32,6 +32,19 @@ telescope.setup {
         ['<esc>'] = actions.close
       },
     },
+    file_ignore_patterns = {
+      '.git/',
+    },
+    vimgrep_arguments = {
+      'rg',
+      '--color=never',
+      '--no-heading',
+      '--with-filename',
+      '--line-number',
+      '--column',
+      '--smart-case',
+      '--hidden', -- Add hidden files to ripgrep selection
+    }
   },
 }
 
